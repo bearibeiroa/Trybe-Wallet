6767cb8101d6887056c991d911ab3b7e167b1674
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-15 20:32:23 +0100

    Criação da rota '/' e formulário de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="login">
+          TrybeWallet
+          <input type="email" name="email" data-testid="email-input" />
+          <input type="text" name="senha" data-testid="password-input" />
+          <button type="button">Entrar</button>
+        </label>
+      </form>
+    );
   }
 }
 
