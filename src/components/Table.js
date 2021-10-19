5fad4666b664527580f0d019e549102c915c0754
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-19 23:28:32 +0100

    Requisito 9 completo

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import TableHead from './TableHead';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-      </table>
+      <>
+        <TableHead />
+        <table>
+          <tbody>
+            { expenses.map((item) => {
+              const {
+                id, description, tag, method, value, currency, exchangeRates,
+              } = item;
+              const exchangeRateInfo = Number(exchangeRates[currency].ask);
+              const exchangeValue = Number(value) * exchangeRateInfo;
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ value }</td>
+                  <td>{ exchangeRates[currency].name }</td>
+                  <td>{ exchangeRateInfo.toFixed(2) }</td>
+                  <td>{ exchangeValue.toFixed(2) }</td>
+                  <td>Real</td>
+                  <td>
+                    <button type="button">Editar</button>
+                    <button type="button">Excluir</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  expensesInfo: state.wallet.expenses,
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
 });
 
-export default connect(mapStateToProps, null)(Table);
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object),
+};
+
+Table.defaultProps = {
+  expenses: [],
+};
+
+export default connect(mapStateToProps)(Table);
---
+++ src/components/TableHead.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class TableHead extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+      </table>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  expensesInfo: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(TableHead);
